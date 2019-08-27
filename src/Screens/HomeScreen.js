a13bbf9c93c6141b0c92c227e3003b4ed26be834
Shubham Singla <shubhamsinglars@gmail.com>
2019-08-27 20:32:25 +0530

    Folder Structure and HomeScreen is added

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+import {Text} from 'native-base';
+
+class HomeScreen extends Component{
+
+  render(){
+    return(
+      <Text>HomeScreen</Text>
+    )
+  }
+}
+
+export default HomeScreen;
